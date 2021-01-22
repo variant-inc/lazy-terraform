7b79315435797445497d1fae59aad2512eb79c22
Samir Alyateem <salyateem@USXVAR-M3LAML85.attlocal.net>
2021-01-22 10:39:26 -0500

    Added description to terraform variables

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,49 +1,60 @@
 variable "zone" {
   type = string
+  description = "Default zone for AWS provider"
 }
 
 variable "profile" {
   type = string
+  description = "Credentials profile for AWS provider"
 }
 
 variable "ec2_instance_name" {
   type    = string
   default = "name-not-provided"
+  description = "Name given to ec2 instance. Used as prefix to other resources"
 }
 
 variable "instance_purpose" {
   type = string
+  description = "Purpose of the instance; used for tags"
 }
 
 variable "instance_owner" {
   type = string
+  description = "Owner of the instance; used for tags"
 }
 
 variable "ami_id" {
   type = string
+  description = "ID of the AMI of the instance"
 }
 
 variable "associate_public_ip_address" { 
   type = bool
   default = false
+  description = "Attach public IP to instance?"
 }
 
 variable "ec2_instance_type" {
   type    = string
   default = "t2.small"
+  description = "Machine type of the instance"
 }
 
 variable "ebs_optimized" {
   type = bool
   default = true
+  description = "Optomize instance for EBS?"
 }
 
 variable "vpc_id" {
   type = string
+  description = "ID of VPC to attach security group to"
 }
 
 variable "subnet_type" {
   type = string
+  description = "One of: public or private"
 }
 
 variable "security_group_rules_data" {
@@ -56,22 +67,27 @@ variable "security_group_rules_data" {
       cidr_blocks = list(string)
     })
   )
+  description = "Ingress/Egress rules for the security group"
 }
 
 variable "ebs_volume_size" {
   type = number
+  description = "Size of EBS volume in GB"
 }
 
 variable "kms_key_id" {
   type = string
+  description = "KMS Key for disk encryption"
 }
 
 variable "ebs_vol_type" {
   type = string
+  description = "Volume type of EBS instance. One of: standard, gp2, gp3, io1, io2, sc1 or st1"
 }
 
 variable "ebs_device_name" {
   type = string
   default = "/dev/sdh"
+  description = "Device name given to EBS volume of the instance"
 }
 
