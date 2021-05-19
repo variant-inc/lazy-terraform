b3ea1c406424bcbea1de79e6eae75ccded1dcd06
Naveen <naveenp@drivevariant.com>
2021-05-19 17:13:26 -0400

    Adding linter fix by formatting json

--- s3/run.sh
+++ s3/run.sh
@@ -3,13 +3,8 @@
 set -e
 echo "Invoking create bucket end point"
 
-tags="{"created-by" : "lazy-terraform", \
-      "owner" : $6,"purpose" : $7,"team" : $8 \
-      "/octopus/project_name" : $10, "/octopus/space" : $9 \
-    }"
-
 wget --content-on-error -O - --header="Content-Type:application/json" --header='x-api-key: '"$2"'' \
  --post-data '{"options": { "Bucket" : "'"$5"'"}, 
- "CreateBucketConfiguration": { "LocationConstraint":"'"$3"'" }, "tags": "'"$tags"'" }' "$1"/profiles/devops/s3
+ "CreateBucketConfiguration": { "LocationConstraint":"'"$3"'" }, "tags": { "owner" : "'"$6"'","purpose" : "'"$7"'","team" : "'"$8"'" }}' "$1"/profiles/devops/s3
 
 echo "Response Body: $HTTP_RESPONSE"
