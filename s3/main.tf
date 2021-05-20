9bdfc9dce68a1deaeb8d52ffde067cd38a5c1acb
Naveen <naveenp@drivevariant.com>
2021-05-20 13:08:37 -0400

    Fixing linter issue

--- s3/main.tf
+++ s3/main.tf
@@ -14,13 +14,13 @@ resource "null_resource" "lazy_s3_api" {
   }
 
  provisioner "local-exec" {
-    working_dir = "${path.module}"
+    working_dir = path.module
     command = "./run.sh ${self.triggers.lazy_api_host} ${self.triggers.lazy_api_key} ${self.triggers.region} ${self.triggers.profile} ${self.triggers.bucket_name} ${self.triggers.owner} ${self.triggers.purpose} ${self.triggers.team} ${self.triggers.octopus_project_space} ${self.triggers.octopus_project_name}"
   }
 
  provisioner "local-exec" {
     when        = destroy
-    working_dir = "${path.module}"
+    working_dir = path.module
     command = "./cleanup.sh ${self.triggers.lazy_api_host} ${self.triggers.lazy_api_key} ${self.triggers.profile} ${self.triggers.bucket_name}"
   }
 }
