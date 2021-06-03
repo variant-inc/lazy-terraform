a87cf6bb1aa43714744ba41a0d1a718a834dad16
Naveen <naveenp@drivevariant.com>
2021-06-03 14:58:39 -0400

    Adding intrepreter

--- s3/main.tf
+++ s3/main.tf
@@ -17,12 +17,14 @@ resource "null_resource" "lazy_s3_api" {
   provisioner "local-exec" {
     working_dir = path.module
     command = "source env.sh && ./run.sh ${var.lazy_api_host} ${var.lazy_api_key} ${var.region} ${var.profile} ${self.triggers.bucket_name} ${var.owner} ${var.purpose} ${var.team} ${var.octopus_project_space} ${var.octopus_project_name} ${var.role_arn}"
+    interpreter = ["/bin/bash", "-c"]
   }
 
   provisioner "local-exec" {
     when        = destroy
     working_dir = path.module
     command = "source env.sh && ./cleanup.sh ${self.triggers.bucket_name}"
+    interpreter = ["/bin/bash", "-c"]
   }
   depends_on = [ local_file.environment_sh]
 }
