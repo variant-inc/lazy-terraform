34e242196a126710b0be8b55c16cc97577a6666f
Naveen <naveenp@drivevariant.com>
2021-06-09 15:17:12 -0400

    removing backend attributes

--- s3/config.tf
+++ s3/config.tf
@@ -1,11 +1,5 @@
 terraform {
-    required_version = "~> 0.15.0"
+  required_version = "~> 0.15.0"
   backend "s3" {
-    profile         = "108141096600_AWSAdministratorAccess"
-    bucket          = "lazy-tf-state20210107203535113800000001"
-    key             = "s3/default9"
-    region          = "us-west-2"
-    dynamodb_table  = "lazy_tf_state"
-    encrypt         = false
   }
 }
