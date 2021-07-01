dc15dd13ec5719c7b148156024c12ecee82ef4a1
Vibin Daniel <vibind@drivevariant.com>
2021-07-01 13:53:01 -0400

    [CLEANUP] Remove unused terraform

--- iaac_policy/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_iam_policy" "policy" {
-  count       = length(var.iam_policy_json)
-  name        = element(var.iam_policy_json, count.index)
-  path        = var.iam_path
-  description = "Policy for EKS to run ${replace(element(var.iam_policy_json, count.index), "-policy", "")}"
-
-  policy      = replace(file("policies/${element(var.iam_policy_json, count.index)}.json"), "aws_account_number", var.aws_account_number)
-}
