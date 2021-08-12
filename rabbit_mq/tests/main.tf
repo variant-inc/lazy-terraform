7d626aefd7d66aadfa11744ad6ec1018a44c5d17
Hitesh Patel <hpatel@hiteshp.me>
2021-08-12 12:37:53 -0400

    rabbit_mq - add test

---
+++ rabbit_mq/tests/main.tf
@@ -0,0 +1,9 @@
+module "rabbit_mq" {
+  source = "../"
+
+  broker_name         = var.broker_name
+  publicly_accessible = var.publicly_accessible
+  
+  user_tags           = var.user_tags
+  octopus_tags        = var.octopus_tags
+}
---
+++ rabbit_mq/tests/variables.tf
@@ -0,0 +1,15 @@
+variable "octopus_tags" {
+  type = map(string)
+}
+
+variable "broker_name" {
+  type = string
+}
+
+variable "user_tags" {
+  type = map(string)
+}
+
+variable "publicly_accessible" {
+  type = bool
+}
