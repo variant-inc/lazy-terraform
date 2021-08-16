3a223f8e06ba575730a9c02ce8f9949b2889fd25
Naveen <naveenp@drivevariant.com>
2021-08-16 16:32:57 -0400

    new params fix

--- submodules/tags/main.tf
+++ submodules/tags/main.tf
@@ -12,9 +12,9 @@ locals {
     "aws/account_number"      = data.aws_caller_identity.current.account_id
     "octopus/project"         = var.octopus_tags.project
     "octopus/space"           = var.octopus_tags.space
-    "octopus/environment"     = var.octopus_tags.environment
-    "octopus/project_group"   = var.octopus_tags.project_group
-    "octopus/release_channel" = var.octopus_tags.release_channel
+    "octopus/environment"     = lookup(var.octopus_tags, "environment", null)
+    "octopus/project_group"   = lookup(var.octopus_tags, "project_group", null)
+    "octopus/release_channel" = lookup(var.octopus_tags, "release_channel", null)
   }
 
   tags = merge(local.common_tags, local.user_tags)
