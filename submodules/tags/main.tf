5199f1f6d214e2bb7431b06ba03de1bf2eb40f09
Samir Alyateem <31292258+salyateem13@users.noreply.github.com>
2021-08-17 17:06:48 -0400

    Update main.tf

--- submodules/tags/main.tf
+++ submodules/tags/main.tf
@@ -12,9 +12,9 @@ locals {
     "aws/account_number"      = data.aws_caller_identity.current.account_id
     "octopus/project"         = var.octopus_tags.project
     "octopus/space"           = var.octopus_tags.space
-    "octopus/environment"     = lookup(var.octopus_tags, "environment", null)
-    "octopus/project_group"   = lookup(var.octopus_tags, "project_group", null)
-    "octopus/release_channel" = lookup(var.octopus_tags, "release_channel", null)
+    "octopus/environment"     = lookup(var.octopus_tags, "environment", '')
+    "octopus/project_group"   = lookup(var.octopus_tags, "project_group", '')
+    "octopus/release_channel" = lookup(var.octopus_tags, "release_channel", '')
   }
 
   tags = merge(local.common_tags, local.user_tags)
