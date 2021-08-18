542c625e1a46ebf6b2cc34fb50faa485a5ab800f
Samir Alyateem <31292258+salyateem13@users.noreply.github.com>
2021-08-17 21:32:39 -0400

    Update main.tf (#52)
    * reverting changes
    
    Co-authored-by: Naveen <naveenp@drivevariant.com>

--- submodules/tags/main.tf
+++ submodules/tags/main.tf
@@ -8,13 +8,10 @@ locals {
   }
 
   common_tags = {
-    "Name"                    = var.name
-    "aws/account_number"      = data.aws_caller_identity.current.account_id
-    "octopus/project"         = var.octopus_tags.project
-    "octopus/space"           = var.octopus_tags.space
-    "octopus/environment"     = lookup(var.octopus_tags, "environment", null)
-    "octopus/project_group"   = lookup(var.octopus_tags, "project_group", null)
-    "octopus/release_channel" = lookup(var.octopus_tags, "release_channel", null)
+    "Name"               = var.name
+    "aws/account_number" = data.aws_caller_identity.current.account_id
+    "octopus/project"    = var.octopus_tags.project
+    "octopus/space"      = var.octopus_tags.space
   }
 
   tags = merge(local.common_tags, local.user_tags)
