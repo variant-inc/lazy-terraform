f37979471b3ee9fa4472c0762966d96192f3e694
Naveen <naveenp@drivevariant.com>
2021-12-22 14:02:24 -0500

    minor fixes to s3 tags

--- s3/scripts/tags.ps1
+++ s3/scripts/tags.ps1
@@ -40,10 +40,21 @@ catch
   Write-Information "Bucket $BUCKET_NAME already exists"
 }
 
+
 $TAGS
+
+$hashtable = @{}
+
+(ConvertFrom-Json $TAGS).psobject.properties | Foreach { $hashtable[$_.Name] = $_.Value }
+
+
+$body +=@{}
+$body["tags"] = $hashtable
+$body | ConvertTo-Json -Depth 100 -Compress
+
 
 Write-Output "Lazy API Update URL $lazyS3UpdateUrl"
 $Response = Invoke-RestMethod -Uri $lazyS3UpdateUrl `
-  -Headers $headers -Method PUT -Body $TAGS
+  -Headers $headers -Method PUT -Body $body | ConvertTo-Json -Depth 100 -Compress
 
 $Response | ConvertTo-Json
